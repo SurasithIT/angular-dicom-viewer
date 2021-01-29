45b57ed3bfb8f96397c7431b9c1530bdfa707311
Surasith.k <Surasith.k@kabintech.co.th>
2021-01-29 14:10:04 +0700

    Add DicomViewerComponent and install dependecy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DicomViewerComponent } from './dicom-viewer/dicom-viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DicomViewerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/dicom-viewer/dicom-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dicom-viewer',
+  templateUrl: './dicom-viewer.component.html',
+  styleUrls: ['./dicom-viewer.component.sass']
+})
+export class DicomViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
